54a563693bef7fa7ffb2e85de8d8d6f7cb01d8dc
mnlazs <mnl.azs@gmail.com>
2024-02-15 17:41:21 -0600

    updated

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -9,5 +9,12 @@ int sub(int a, int b) {
 String showFunc(int a, int b) {
   int sumResult = add(a, b);
   int subResult = sub(a, b);
-  return 'Sum of $a and $b is $sumResult and subtraction is $subResult';
+  return 'Add $a + $b = $sumResult\nSub $a - $b = $subResult';
+}
+
+void main() {
+  int a = 8;
+  int b = 5;
+  String message = showFunc(a, b);
+  print(message);
 }
