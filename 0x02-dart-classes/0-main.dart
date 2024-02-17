0ec883b0f0c6f06ba85e20eb2548d6a4e00709fa
mnlazs <mnl.azs@gmail.com>
2024-02-17 09:01:08 -0600

    updated

---
+++ 0x02-dart-classes/0-main.dart
@@ -0,0 +1,7 @@
+import '0-class.dart';
+
+void main() {
+  final firstPer = User(); // Crea una instancia de User
+  firstPer.name = "Youssef Belhadj"; // Asigna un nombre a la propiedad 'name'
+  print(firstPer.showName()); // Imprime "Hello Youssef Belhadj"
+}
