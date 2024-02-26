3bc2cd059d9c9ca6ef121bc8157655128b516cb4
mnlazs <mnl.azs@gmail.com>
2024-02-25 20:48:16 -0600

    updated

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -3,34 +3,32 @@ import '4-util.dart';
 
 Future<double> calculateTotal() async {
   try {
-    // Obtiene los datos del usuario.
+    // Obtén los datos del usuario
     final String userDataJson = await fetchUserData();
     final Map<String, dynamic> userData = json.decode(userDataJson);
     final String userId = userData['id'];
 
-    // Obtiene las órdenes del usuario.
+    // Obtén los pedidos del usuario
     final String userOrdersJson = await fetchUserOrders(userId);
     final List<dynamic> userOrders = json.decode(userOrdersJson);
 
-    double totalPrice = 0.0;
+    double totalPrice = 0;
 
-    // Itera sobre cada producto en las órdenes del usuario para obtener su precio.
-    for (var product in userOrders) {
+    // Calcula el precio total iterando sobre los pedidos del usuario
+    for (String product in userOrders) {
       final String productPriceJson = await fetchProductPrice(product);
       final double productPrice = json.decode(productPriceJson);
       totalPrice += productPrice;
     }
 
-    // Retorna el precio total de los ítems.
     return totalPrice;
   } catch (error) {
-    // Si ocurre un error en cualquier parte del proceso, retorna -1.
-    print('error caught: $error');
-    return -1.0;
+    // Manejo de errores retornando -1 en caso de error
+    return -1;
   }
 }
 
 void main() async {
-  final total = await calculateTotal();
+  final double total = await calculateTotal();
   print(total);
 }
